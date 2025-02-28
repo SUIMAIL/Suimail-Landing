db2d88c43742af50ca6a59898b3d0596b00d5d00
Elite-tch <elite6461@gmail.com>
2025-02-28 15:00:18 +0100

    error

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,6 +1,6 @@
 
 import Footer from '../components/LandingPage/Footer';
-import Signup from '../components/CreateAccount/SignUp';
+import Signup from '../components/CreateAccount/signup';
 export default function SignUpPage() {
     return (
         <div>
